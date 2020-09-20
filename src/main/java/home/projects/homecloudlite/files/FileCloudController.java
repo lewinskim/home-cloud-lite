315810147e395b4333908af21ff797b395abd316
lewinskim <lewinski.mar@gmail.com>
2020-09-20 12:50:15 +0200

    Added serving file

--- src/main/java/home/projects/homecloudlite/files/FileCloudController.java
+++ src/main/java/home/projects/homecloudlite/files/FileCloudController.java
@@ -1,9 +1,14 @@
 package home.projects.homecloudlite.files;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @RestController
 public class FileCloudController {
@@ -15,7 +20,14 @@ public class FileCloudController {
     }
 
     @GetMapping("/files")
-    public List<String> allFiles() {
+    public Stream<Path> allFiles() {
         return fileService.loadHomeDirectoryFiles();
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = fileService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 }
--- src/main/java/home/projects/homecloudlite/files/FileService.java
+++ src/main/java/home/projects/homecloudlite/files/FileService.java
@@ -1,12 +1,14 @@
 package home.projects.homecloudlite.files;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 class FileService {
@@ -19,15 +21,29 @@ class FileService {
         init();
     }
 
-    public List<String> loadHomeDirectoryFiles() {
+    public Stream<Path> loadHomeDirectoryFiles() {
         try {
             return Files.walk(rootAppDirectory, 1)
-                    .map(path -> path.getFileName().toString())
-                    .filter(filename -> !filename.equals(APP_ROOT_DIRECTORY_NAME))
-                    .collect(Collectors.toList());
+                    .filter(filename -> !filename.equals(rootAppDirectory))
+                    .map(rootAppDirectory::relativize);
         } catch (IOException e) {
             throw new IllegalStateException("Could not load files ", e);
         }
+    }
+
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = rootAppDirectory.resolve(filename);
+            UrlResource urlResource = new UrlResource(file.toUri());
+            if (urlResource.exists() || urlResource.isReadable()) {
+                return urlResource;
+            } else {
+                throw new IllegalStateException("Could not read file " + filename);
+            }
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Could not read file " + filename, e);
+        }
+
     }
 
     private void init() {
